1e21b0a673e1d82de01b3cfda1784d23bbcc1ab5
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-10-20 10:42:05 +0700

    add footer and refactor news section

---
+++ resource/js/main.js
@@ -0,0 +1,15 @@
+for(let i=0;i<5;i++) {
+    $('#newsList').append(
+        ' \
+            <div class="col-xl-3 col-md-4 col-sm-6 col-12 my-1"> \
+                <div class="card-news"> \
+                    <a href="/" class="card-news-img mb-4"> \
+                        <img src="/resource/img/news1.jpg" alt=""> \
+                    </a> \
+                    <a href="/" class="card-news-title">Chú chó \'nghiện\' giao thông công cộng.</a> \
+                    <p class="card-news-content text-secondary mb-0">Trong chuyến phà chở khách đông đúc ở Istanbul, mọi con mắt đều hướng về một chú chó đang ngắm nhìn phong cảnh từ cửa sổ.</p> \
+                </div> \
+            </div> \
+        '
+    )
+}
