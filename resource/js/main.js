6054b848ad3599f17a2847bf1ddba78482cc0721
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-10-29 13:38:30 +0700

    add top10 violation chart

--- resource/js/main.js
+++ resource/js/main.js
@@ -68,9 +68,9 @@ $('button[data-city]').each((index, btn) => {
     }
 })
 
-var canvas = document.getElementById('chart-violation-yearly');
+var canvasViolationYearly  = document.getElementById('chart-violation-yearly');
 
-if (canvas) var chartViolationYearly = new Chart(canvas.getContext('2d'), {
+if (canvasViolationYearly ) var chartViolationYearly = new Chart(canvasViolationYearly.getContext('2d'), {
     type: 'line',
     data: {
         labels: ['2016', '2017', '2018', '2019', '2020', '2021'],
@@ -84,6 +84,91 @@ if (canvas) var chartViolationYearly = new Chart(canvas.getContext('2d'), {
     },
     options: {
         responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    font: {
+                        family: "'Inter', sans-serif",
+                        size:16,
+                    }
+                }
+            },
+            title: {
+                display: false,
+                text: ''
+            },
+            tooltip: {
+                callbacks:{
+                    label: function(context) {
+                        return `Số vụ vi phạm: ${context.formattedValue} nghìn vụ`;
+                    },
+                    title: function(context) {
+                        return `Năm ${context[0].label}`;
+                    },
+                },
+                bodyFont: {
+                    family: "'Inter', sans-serif",
+                    size:14,
+                },
+                titleFont: {
+                    family: "'Inter', sans-serif",
+                    size:16,
+                },
+                usePointStyle: false,
+                intersect: false,
+                displayColors: false,
+            },
+        },
+        animations: {
+            radius: {
+                duration: 500,
+                easing: 'easeInQuad',
+                from: 5,
+                to: 15,
+                loop: true
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    font: {
+                        family: "'Inter', sans-serif",
+                        size:14,
+                    },
+                    color: '#1C2833',
+                }
+            },
+            y: {
+                ticks: {
+                    font: {
+                        family: "'Inter', sans-serif",
+                        size:14,
+                    },
+                    color: '#1C2833'
+                }
+            }
+        }
+    }
+});
+
+var canvasTop10Violation  = document.getElementById('chart-top10-violation');
+
+if (canvasTop10Violation ) var canvasTop10Violation = new Chart(canvasTop10Violation.getContext('2d'), {
+    type: 'bar',
+    data: {
+        labels: ['Hồ Chí Minh', 'Hà Nội', 'Hải Phòng', 'Đà Nẵng', 'Khánh Hòa', 'Nam Định', 'Hưng Yên', 'Ninh Bình', 'Hải Dương', 'Phú Thọ',],
+        datasets: [{
+            label: 'Số vụ vi phạm (nghìn vụ)',
+            data: [2, 4, 9, 6, 8, 10, 1, 4, 7, 5],
+            backgroundColor: 'rgba(35, 155, 86, .5)',
+            borderColor: '#239B56',
+            borderWidth: 2
+        }]
+    },
+    options: {
+        responsive: true,
+        indexAxis: 'y',
         plugins: {
             legend: {
                 position: 'bottom',
